4a397c5c32192fb652b2fe00bd20e75cbc45587a
jamalomosun <omosunj@carleton.edu>
2024-02-19 00:37:02 -0600

    modified csslab files

--- csslab/javascript-example/javascript-example/js-example.py
+++ csslab/javascript-example/javascript-example/js-example.py
@@ -9,6 +9,10 @@ def welcome():
     message = message + " This text was produced by concatenating strings in Python!"
     return render_template("homepage.html", someText = message)
 
+@app.route('/Hello')
+def hello():
+    message = "Welcome good sir. It is time to go to sleep now"
+    return render_template("sleepy.html", messText= message)
+
 if __name__ == '__main__':
-    my_port = 5000
     app.run(host='0.0.0.0', port = 5130) 
---
+++ csslab/javascript-example/javascript-example/static/shlumped.js
@@ -0,0 +1,3 @@
+function bedTime() {
+    
+}
