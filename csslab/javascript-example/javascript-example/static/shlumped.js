74f86b175bcce7ee54dc0aa3396a71fcc9c23a41
jamalomosun <omosunj@carleton.edu>
2024-02-26 14:58:02 -0600

    I tried to fix things

--- csslab/javascript-example/javascript-example/static/shlumped.js
+++ csslab/javascript-example/javascript-example/static/shlumped.js
@@ -1,7 +1,5 @@
-function bedTime() {
-    the_heading = document.getElementById("tired");
-    the_heading.style.color = "red";
-}
 function goToSleep() {
 	sleepyTime = document.write("The time is " + Date() + " you should probably go to bed brother"); 
+    the_heading = document.getElementById("tired");
+    the_heading.style.color = "red";
 }
