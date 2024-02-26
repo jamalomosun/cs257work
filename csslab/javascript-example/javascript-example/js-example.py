9aba19b902e039d405efac00d32ee1786f522695
jamalomosun <omosunj@carleton.edu>
2024-02-26 14:40:46 -0600

    I tried to fix things

--- csslab/javascript-example/javascript-example/js-example.py
+++ csslab/javascript-example/javascript-example/js-example.py
@@ -8,11 +8,5 @@ def welcome():
     message = "Welcome good sir. It is time to go to sleep now."
     return render_template("sleepy.html", messText= message)
 
-
-@app.route('/Hello')
-def hello():
-    message = "Welcome good sir. It is time to go to sleep now"
-    return render_template("sleepy.html", )
-
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port = 5130) 
